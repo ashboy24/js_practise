c1a8200c4705541efd07018ce91b0ae3ef38ff97
Aseesh Sai <93883226+ashboy24@users.noreply.github.com>
2022-07-01 11:36:16 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,57 @@
+/*
+Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.*
+Print the string on the browser console using console.log()*
+Print the length of the string on the browser console using console.log()*
+Change all the string characters to capital letters using toUpperCase() method*
+Change all the string characters to lowercase letters using toLowerCase() method*
+Cut(slice) out the first word of the string using substr() or substring() method*
+Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+Check if the string contains a word Script using includes() method*
+Split the string into an array using split() method*
+Split the string 30 Days Of JavaScript at the space using split() method*
+'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.*
+What is character at index 15 in '30 Days Of JavaScript' string ? Use charAt() method.*
+What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()*
+Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript*
+Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.*
+Use indexOf to find the position of the first occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Use search to find the position of the first occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.*
+Use startsWith() method with the string 30 Days Of JavaScript and make the result true*
+Use endsWith() method with the string 30 Days Of JavaScript and make the result true*
+Use match() method to find all the a’s in 30 Days Of JavaScript*
+Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript' *
+Use repeat() method to print 30 Days Of JavaScript 2 times*
+*/
+
+/*
+let challenge;
+challenge = "30 Days of JavaScript";
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substr(3, 4));
+console.log(challenge.includes('Script'));
+console.log(challenge.split(' '));
+console.log(challenge.replace("JavaScript", "Python"));
+console.log(challenge.charAt(15));
+let string = '   30 Days Of JavaScript   '
+
+console.log(string)
+console.log(string.trim(' '))
+
+let firstName = ' Asabeneh '
+
+console.log(firstName)
+console.log(firstName.trim())  // still removes spaces at the beginning and the end of the string
+
+let string1 = '30 Days Of JavaScript'
+console.log(string1.charCodeAt(3))        // D ASCII number is 68
+
+let lastIndex = string1.length - 1
+console.log(string1.charCodeAt(lastIndex)) // t ASCII is 116
+
+*/
