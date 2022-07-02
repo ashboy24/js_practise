13c56c42c9559b037db351b3631d2c901fb3b797
Aseesh Sai <93883226+ashboy24@users.noreply.github.com>
2022-07-02 22:19:36 +0530

    Add files via upload

---
+++ day3,4,5.js
@@ -0,0 +1,148 @@
+/*
+const firstList = [1, 2 ,3];
+const secondList = [4, 5, 6];
+const thirdList = firstList.concat(secondList);
+console.log(thirdList);
+console.log(thirdList.length);
+console.log(thirdList.indexOf(5));
+*/
+
+/*
+// let us check if banana exists in that array: 
+const fruits = ['banana', 'orange', 'mango', 'lemon'];
+let index = fruits.indexOf('banana');
+if(index == -1)
+{
+    console.log('This fruit does not exist in the array');
+}
+else
+{
+    console.log('This fruit does exist in the given array');
+}
+
+// We can use also ternary here
+
+index == -1 ? console.log('This fruit does not exist in the array') : console.log('This fruit does exist in the array');
+//let us check if an avocado exist in the array
+
+let indexOfAvacado = fruits.indexOf('avocado');
+if (indexOfAvacado == -1)
+{
+    console.log('This fruit does not exist in the array');
+}
+else
+{
+    console.log('This fruit does exist in the array');
+}
+*/
+
+/*
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+*/
+
+
+/*
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.join()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names.join('')) //AsabenehMathiasEliasBrook
+console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+] // List of web technologies
+
+console.log(webTechs.join())       // "HTML,CSS,JavaScript,React,Redux,Node,MongoDB"
+console.log(webTechs.join(' # '))  // "HTML # CSS # JavaScript # React # Redux # Node # MongoDB"
+*/
+
+/*
+  const numbers = [1,2,3,4,5]
+
+  console.log(numbers.slice()) // -> it copies all  item
+  console.log(numbers.slice(0)) // -> it copies all  item
+  console.log(numbers.slice(0, numbers.length)) // it copies all  item
+  console.log(numbers.slice(1,4)) // -> [2,3,4] // it doesn't include the ending position
+*/
+
+/*  const numbers = [1, 2, 3, 4, 5, 6]
+    numbers.splice(3, 3, 7, 8, 9)
+  console.log(numbers.splice(3, 3, 7, 8, 9))  // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+*/
+
+/*
+const numbers = [1, 2, 3, 4, 5]
+numbers.push(6)
+console.log(numbers) // -> [1,2,3,4,5,6]
+
+numbers.pop() // -> remove one item from the end
+console.log(numbers) // -> [1,2,3,4,5]
+*/
+
+/*
+let fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.push('apple')
+console.log(fruits)    // ['banana', 'orange', 'mango', 'lemon', 'apple']
+
+fruits.push('lime')
+console.log(fruits)   // ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+*/
+
+/*
+const numbers = [1, 2, 3, 4, 5]
+numbers.reverse() // -> reverse array order
+console.log(numbers) // [5, 4, 3, 2, 1]
+
+numbers.reverse()
+console.log(numbers) // [1, 2, 3, 4, 5]
+*/
+
+/*
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+webTechs.sort()
+console.log(webTechs) // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+
+webTechs.reverse() // after sorting we can reverse it
+console.log(webTechs) // ["Redux", "React", "Node", "MongoDB", "JavaScript", "HTML", "CSS"]
+
+*/
+
+/*
+const firstNums = [1, 2, 3]
+const secondNums = [1, 4, 9]
+
+const arrayOfArray =  [[1, 2, 3], [1, 2, 3]]
+console.log(arrayOfArray[0]) // [1, 2, 3]
+
+ const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+ const backEnd = ['Node','Express', 'MongoDB']
+ const fullStack = [frontEnd, backEnd]
+ console.log(fullStack)   // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+ console.log(fullStack.length)  // 2
+ console.log(fullStack[0])  // ["HTML", "CSS", "JS", "React", "Redux"]
+ console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
+*/
